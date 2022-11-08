ef6466bcce34f2e16a11355106ceecef7d095e04
Jon Boone <ipmonger@delamancha.org>
2022-11-07 20:40:55 -0500

    adds startup time measurement

--- early-init.el
+++ early-init.el
@@ -1,2 +1,13 @@
+;; measure startup time
+(defun efs/display-startup-time ()
+  (message "Emacs loaded in %s with %d garbage collections."
+           (format "%.2f seconds"
+                   (float-time
+                    (time-subtract after-init-time before-init-time)))
+           gcs-done))
+
+(add-hook 'emacs-startup-hook #'efs/display-startup-time)
+
+
 ;; Disable package.el in favor of straight.el
 (setq package-enable-at-startup nil)
